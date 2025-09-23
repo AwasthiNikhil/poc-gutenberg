4b2031413b7d1973d4f7ece388c8c58916a195e7
AwasthiNikhil <nikhilawasthi944@gmail.com>
2025-09-23 17:05:17 +0545

    navbar ui improvement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,56 @@
 import React from 'react'
 import { Button } from '@wordpress/components';
 import { useState } from '@wordpress/element';
+import {
+    __experimentalToggleGroupControl as ToggleGroupControl,
+    __experimentalToggleGroupControlOption as ToggleGroupControlOption,
+} from '@wordpress/components';
 
 const Navbar = ({ handleInserterOpen, handleSettingsPanelOpen }) => {
     return (
-        <>
+        <div className='navbar'>
             <div>
-                <Button onClick={handleInserterOpen}>Inserter</Button>
+                <ToggleGroupControl>
+                    <Button onClick={handleInserterOpen}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M11 12.5V17.5H12.5V12.5H17.5V11H12.5V6H11V11H6V12.5H11Z"></path></svg>
+                    </Button>
 
-                <div>Undo</div>
-                <div>Redo</div>
-                <div>Document Overview</div>
+                    <Button>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M18.3 11.7c-.6-.6-1.4-.9-2.3-.9H6.7l2.9-3.3-1.1-1-4.5 5L8.5 16l1-1-2.7-2.7H16c.5 0 .9.2 1.3.5 1 1 1 3.4 1 4.5v.3h1.5v-.2c0-1.5 0-4.3-1.5-5.7z"></path></svg>
+                    </Button>
+
+                    <Button>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M15.6 6.5l-1.1 1 2.9 3.3H8c-.9 0-1.7.3-2.3.9-1.4 1.5-1.4 4.2-1.4 5.6v.2h1.5v-.3c0-1.1 0-3.5 1-4.5.3-.3.7-.5 1.3-.5h9.2L14.5 15l1.1 1.1 4.6-4.6-4.6-5z"></path></svg>
+                    </Button>
+
+                    <Button>
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" aria-hidden="true" focusable="false"><path d="M3 6h11v1.5H3V6Zm3.5 5.5h11V13h-11v-1.5ZM21 17H10v1.5h11V17Z"></path></svg>
+                    </Button>
+
+                </ToggleGroupControl>
+
             </div>
             <div>
-                <div>
+                <ToggleGroupControl>
                     Commands
-                </div>
+                </ToggleGroupControl>
             </div>
             <div>
-                <div>
-                    Save draft
-                </div>
-                <div>
-                    <Button onClick={handleSettingsPanelOpen}>Settings</Button>
-                </div>
-                <div>
-                    Publish
-                </div>
+                <ToggleGroupControl>
+                    <div>
+                        Save draft
+                    </div>
+                    <div>
+                        <Button onClick={handleSettingsPanelOpen}>
+                            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path fill-rule="evenodd" clip-rule="evenodd" d="M18 4H6c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-4 14.5H6c-.3 0-.5-.2-.5-.5V6c0-.3.2-.5.5-.5h8v13zm4.5-.5c0 .3-.2.5-.5.5h-2.5v-13H18c.3 0 .5.2.5.5v12z"></path></svg>
+                        </Button>
+                    </div>
+                    <div>
+                        Publish
+                    </div>
+                </ToggleGroupControl>
             </div>
-        </>
+        </div>
     )
 }
 
