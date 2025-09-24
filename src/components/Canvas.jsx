17a42aae4124849a70b5ee879256f2e44b95dc58
AwasthiNikhil <nikhilawasthi944@gmail.com>
2025-09-24 22:49:48 +0545

    title bar added

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BlockCanvas} from '@wordpress/block-editor';
+import {BlockCanvas, BlockList} from '@wordpress/block-editor';
+import { PlainText } from '@wordpress/block-editor';
 
 // canvas styles
 import componentsStyles from '@wordpress/components/build-style/style.css?raw';  
@@ -14,9 +15,30 @@ const contentStyles = [
 
 const Canvas = () => {
     return (
-        <BlockCanvas
-            height="100%"
-            styles={contentStyles} />
+        // <BlockCanvas
+        //     height="100%"
+        //     styles={contentStyles} />
+<BlockCanvas  
+            height="100%"  
+            styles={contentStyles}  
+        >  
+            <div>  
+                <PlainText 
+                placeholder="Title..."
+                onChange={()=>{}}
+                style={{
+                  textAlign: "center",
+                  fontSize: "24px",
+                  fontWeight: "bold",
+                  border: "none",
+                  outline: "none",
+                  padding: "12px 0"
+                }}
+                />
+            </div>  
+            <BlockList />  
+        </BlockCanvas>  
+
     )
 }
 
